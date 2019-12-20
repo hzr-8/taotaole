e4141132e547c4967f86ab41b6762f8a5fd9167e
hzr-8 <1149025485@qq.com>
2019-12-20 14:02:01 +0800

    封装promise对象发送请求

--- utils/request.js
+++ utils/request.js
@@ -1,34 +1,28 @@
 const baseURL = 'https://api.zbztb.cn/api/public/v1/';
 
-
-// myRequest({
-//   url: 'categories',
-//   data: {},
-//   method: 'POST'
-// }).then(res => {
-
-// })
-export const myRequest = (obj) => {
+export const myRequest = (params) => {
   // 显示加载框
   wx.showLoading({
-    title: '疯狂加载中...',
+    title: '网管，好卡啊...',
   });
   return new Promise((resolve, reject) => {
     // 发起请求
     wx.request({
-      ...obj,
-      url: baseURL + obj.url,
+      //结构所有参数
+      ...params,
+      url: baseURL + params.url,
       // 请求成功的回调函数
       success: res => {
+        //对应.then(res=>{})
         resolve(res.data.message);
       },
       // 请求失败的回调函数
-      fail: err => {
-        // console.log('请求失败的业务逻辑', err);
-        reject(err);
+      fail: error => {
+        //对应.catch(error=>{})
+        reject(error);
       },
-      // 请求结束的回调函数，不管成功还是失败都执行
-      complete: res => {
+      // 完成，不管成功还是失败都执行
+      complete: () => {
         // 不管成功还是失败都执行隐藏提示框
         wx.hideLoading();
         // 在手机里面，还有主动调用隐藏加载的API

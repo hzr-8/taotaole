0196fc34010a9a24edec493c21ab37d78fbfc413
hzr-8 <1149025485@qq.com>
2019-12-20 17:07:12 +0800

    使用封装的promise发送请求，添加了下拉刷新功能

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,7 +1,6 @@
 // pages/goods_list/index.js
-import {
-  myRequest
-} from '../../utils/request.js'
+const app = getApp()
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
 
   /**
@@ -9,7 +8,10 @@ Page({
    */
   data: {
     goodsData:{},
-    tabIndex:0
+    tabIndex:0,
+    pagenum:1,
+    query:'',
+    cid:0
   },
   changeTab(e){
     // console.log(e);
@@ -18,24 +20,6 @@ Page({
       tabIndex
     })
   },
-  /*  // 封装列表页请求数据
-   getGoods(obj) {
-
-    myRequest({
-      // url 路径，注意有 goods 
-      url: 'goods/search',
-      // data 请求的参数
-      data: {
-        // cid 从分类页过来的 cid 值
-        // cid: "5"
-        ...obj
-      }
-    }).then(res => {
-      this.setData({
-        goodsData: res
-      })
-    })
-  }, */
 
   /**
    * 生命周期函数--监听页面加载
@@ -44,7 +28,7 @@ Page({
     // console.log(options);
     const {cid,query} = options
     // console.log(cid);
-    wx.request({
+    /* wx.request({
       url:'https://api.zbztb.cn/api/public/v1/goods/search',
       data:{
         query,
@@ -58,11 +42,24 @@ Page({
           goodsData:res.data.message
         })
       }
+    }) */
+    app.myRequest({
+      url:'goods/search',
+      data:{
+        query,
+        cid,
+        pagenum:this.data.goodsData.pagenum,
+        pagesize:10
+      }
+    }).then(res=>{
+      console.log(res);
+      this.setData({
+        goodsData:res,
+        pagenum:res.pagenum,
+        cid,
+        query
+      })
     })
-    /* this.getGoods({
-      // 传递 cid，根据分类 id 查询数据
-      cid
-    }); */
   },
 
   /**
@@ -97,7 +94,22 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    console.log('下拉刷新');
+    app.myRequest({
+      url:'goods/search',
+      data:{
+        query:this.data.query,
+        cid:this.data.cid,
+        pagenum:this.data.pagenum,
+        pagesize:10
+      }
+    }).then(res=>{
+      console.log(res);
+      this.setData({
+        goodsData:res,
+        pagenum:res.pagenum
+      })
+    })
   },
 
   /**

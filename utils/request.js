382e5f6d03e2d6d539129396e50cd554ec10eb2f
hzr-8 <1149025485@qq.com>
2019-12-23 18:58:07 +0800

    添加了请求头，传token

--- utils/request.js
+++ utils/request.js
@@ -1,6 +1,12 @@
 const baseURL = 'https://api.zbztb.cn/api/public/v1/';
 
 export const myRequest = (params) => {
+   // v2.0 如果有 header 就直接使用，没有就赋值空对象
+   params.header = params.header || {};
+   // v2.0 检查 url 的字符串中是否包含了 my/ 路径，如果包含，就在请求头带上token
+   if (params.url.includes('my/')===true){
+     params.header.Authorization = wx.getStorageSync('token');
+   }
   // 显示加载框
   wx.showLoading({
     title: '玩命加载中...',
